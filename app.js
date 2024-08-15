955d108e6a2d55b7e6cd2f0aeebc167a7306312f
ShreyaShahu <123928933+Shreyashahu@users.noreply.github.com>
2024-08-15 19:36:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const button = document.querySelector("button");
+
+const clear = () => {
+    setTimeout(() => {
+        button.classList.remove("effect2");
+    }, 1000);
+};
+
+
+
+button.addEventListener("click", ()=> {
+    button.classList.add("effect1");
+    setTimeout(() => {
+        button.classList.remove("effect1");
+        button.classList.add("effect2");
+        clearInterval();
+    }, 2000);
+});
